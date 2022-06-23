4834409d753f7442d79ad9d40f1b515763274d4e
Rajiv Gopal Singh <rajivgopalsingh@gmail.com>
2022-06-23 17:08:28 +0545

    second

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,13 +1,7 @@
 // ignore_for_file: no_duplicate_case_values, avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/screens/basket/basket_screen.dart';
-import 'package:food_delivery_app/screens/checkout/checkout_screen.dart';
-import 'package:food_delivery_app/screens/delivery_time/delivery_time_screen.dart';
-import 'package:food_delivery_app/screens/filter/filter_screen.dart';
-import 'package:food_delivery_app/screens/restaurant_details/restaurant_details_screen.dart';
 import 'package:food_delivery_app/screens/screen.dart';
-import 'package:food_delivery_app/screens/voucher/voucher_screen.dart';
 
 class AppRouter {
   static Route onGenerateRoute(RouteSettings settings) {
