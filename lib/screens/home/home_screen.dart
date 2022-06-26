06d0f7e318da39cf58f486c8715f28e750837847
Rajiv Gopal Singh <rajivgopalsingh@gmail.com>
2022-06-26 23:13:42 +0545

    adding contenst in HomeScreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,56 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../model/model.dart';
+import '../../widget/widget.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: CustomAppBar(),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: Category.categories.length,
+                itemBuilder: (context, index) {
+                  return CategoryBox(category: Category.categories[index]);
+                },
+              ),
+            ),
+          ),
+          //Promo Box
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 125,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return PromoBox();
+                },
+              ),
+            ),
+          ),
+          FoodSearchBox(),
+        ],
+      )),
+    );
+  }
+
   static const String routeName = '/';
   static Route route() {
     return MaterialPageRoute(
@@ -9,16 +58,43 @@ class HomeScreen extends StatelessWidget {
       settings: const RouteSettings(name: routeName),
     );
   }
+}
 
-  const HomeScreen({Key? key}) : super(key: key);
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text("Home")),
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.person),
+        onPressed: () {},
       ),
-      body: Center(child: SvgPicture.asset('assets/logo.svg')),
+      centerTitle: false,
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Current Location",
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1!
+                .copyWith(color: Colors.white),
+          ),
+          Text(
+            "Nepal, 3 Sitalpati Way",
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(color: Colors.white),
+          ),
+        ],
+      ),
     );
   }
+
+  @override
+  Size get preferredSize => Size.fromHeight(56);
 }
